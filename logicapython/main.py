82f3f91c70eed608e320ac6ae4412b161f7ee3ec
Leidy <lady-2110@hotmail.com>
2022-02-25 20:56:36 -0500

    NTV001 se crea condicinales para verificar el nivel del agua, donde menos de 200 es un alerta e nivel de agua bajo, entre 200 y 400 el nivel del agua esta estable y mayor de 400 el nivel del agua es elevado

--- logicapython/main.py
+++ logicapython/main.py
@@ -1,7 +1,10 @@
-#comentario
-print("Que miedo la guerra")
-
 nivelAgua= int(input("Digita la cantidad de agua de la represa: "))
 print(f"El nivel del agua es {nivelAgua}")
 
-print("oe")
+if(nivelAgua < 200):
+	print("nivel de agua por debajo de los 200")
+elif(nivelAgua >= 200 and nivelAgua < 400):
+	print("nivel de agua adecuado")
+else:
+	print("Nivel del agua elevado mayor a 400")
+
